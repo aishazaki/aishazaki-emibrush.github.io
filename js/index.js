b2686a864eabb00ba2c268f074d7a45350cc4f26
aishazaki <aishazaki3@gmail.com>
2021-08-01 22:41:53 +0300

    comments section

--- js/index.js
+++ js/index.js
@@ -37,4 +37,45 @@ $(document).ready(function(){
             $(".seeMore-btn").text('اخفاء')
         }
     });
-})
+
+    //قسم التعليقات
+    var x = 0,
+    container = $('#comments'),
+    items = container.find('li'),
+    containerHeight = 0,
+    numberVisible = 3,
+    intervalSec = 2000;
+
+    if(!container.find('li:first').hasClass("first")){
+        container.find('li:first').addClass("first");
+    }
+
+    items.each(function(){
+        if(x < numberVisible){
+         containerHeight = containerHeight + $(this).outerHeight();
+         x++;
+        }
+    });
+
+    container.css({ height: containerHeight, overflow: "hidden" });
+    
+    function vertCycle() {
+        var firstItem = container.find('li.first').html();
+            
+        container.append('<li>'+firstItem+'</li>');
+        firstItem = '';
+        container.find('li.first').animate({ "margin-top": "-50px" }, 700, function(){  $(this).remove(); container.find('li:first').addClass("first"); });
+    }
+
+    if(intervalSec < 700){
+    intervalSec = 700;
+    }
+
+    var init = setInterval( vertCycle,intervalSec);
+
+    container.hover(function(){
+            clearInterval(init);
+        }, function(){
+            init = setInterval( vertCycle ,intervalSec)
+    })
+});
